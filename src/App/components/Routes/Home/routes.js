dc2a54a3bfb33b96efdf5b816585938fad3cd625
Diana Gevorgyan <diana_gevorgyan2@edu.aua.am>
2020-02-23 12:07:05 +0400

    updates

--- src/App/components/Routes/Home/routes.js
+++ src/App/components/Routes/Home/routes.js
@@ -1,40 +1,40 @@
-import HomePage from 'views/HomePage/Home';
-import SignUp from 'views/HomePage/SignUp/components/SignUp';
-import SignIn from 'views/HomePage/Signin/Signin';
-import Reset from 'views/HomePage/Reset/Reset';
+import HomePage from "views/HomePage/Home";
+import SignUp from "views/HomePage/SignUp/components/SignUp";
+import SignIn from "views/HomePage/Signin/Signin";
+import Reset from "views/HomePage/Reset/Reset";
 
 const homeRoutes = [
   {
-    path: '/',
-    name: 'HomePage',
+    path: "/",
+    name: "HomePage",
     // icon: Dashboard,
     component: HomePage,
     // layout: '/home',
   },
   // {
-  //   path: '/home',
-  //   name: 'HomePage',
+  //   path: '/about',
+  //   name: 'About Us',
   //   // icon: Dashboard,
-  //   component: HomePage,
+  //   component: About,
   //   // layout: '/home',
   // },
   {
-    path: '/signup',
-    name: 'HomePage',
+    path: "/signup",
+    name: "HomePage",
     // icon: Dashboard,
     component: SignUp,
     // layout: '/home',
   },
   {
-    path: '/signin',
-    name: 'HomePage',
+    path: "/signin",
+    name: "HomePage",
     // icon: Dashboard,
     component: SignIn,
     // layout: '/home',
   },
   {
-    path: '/reset',
-    name: 'Reset',
+    path: "/reset",
+    name: "Reset",
     // icon: Dashboard,
     component: Reset,
     // layout: '/home',
--- src/App/components/Routes/User/routes.js
+++ src/App/components/Routes/User/routes.js
@@ -3,13 +3,11 @@ import Person from "@material-ui/icons/Person";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import MyProfile from "views/MyProfile";
 import Companies from "views/Companies";
-import { useSelector } from "react-redux";
 // import App from 'App/App';
 
-const id = useSelector(state => state.userData.id);
 const dashboardRoutes = [
   {
-    path: `/${id}`,
+    path: "/",
     name: "My Profile",
     icon: Person,
     component: MyProfile,

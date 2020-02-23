96bfc2625dc5fdbef9a447f77ec37f4f828d6510
Diana Gevorgyan <diana_gevorgyan2@edu.aua.am>
2020-02-23 13:35:45 +0400

    commit

--- src/redux/actions/user.actions.js
+++ src/redux/actions/user.actions.js
@@ -6,14 +6,20 @@ const registerUser = user => dispatch => {
     type: userConstants.REGISTER_REQUEST,
   });
   makePost("/api/v1/signup", {}, user)
-    .then(user => dispatch({ type: userConstants.LOGIN_REQUEST, user }))
+    .then(user => {
+      localStorage.setItem("token", user.token),
+        dispatch({ type: userConstants.LOGIN_REQUEST, user });
+    })
     .catch(e => console.error(e));
 };
 
 const login = userCredentials => dispatch => {
   dispatch({ type: userConstants.LOGIN_REQUEST, userCredentials });
   makePost("/api/v1/login", {}, userCredentials)
-    .then(user => dispatch({ type: userConstants.GET_REQUEST, user }))
+    .then(user => {
+      localStorage.setItem("token", user.token),
+        dispatch({ type: userConstants.GET_REQUEST, user });
+    })
 
     .catch(e => console.error(e));
 };

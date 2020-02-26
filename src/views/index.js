9c6839db25b17a3e4c44062aba3b69efce52f2fe
grigoryanmane <manegrigoryan1995@gmail.com>
2020-02-27 01:31:06 +0400

    The requests are working

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,7 @@
-import MyProfile from "./MyProfile";
+import MyProfile from "../../src/views/MyProfile";
 import Home from "./Home";
 import Profile from "./Profile";
-import Notifications from "./Notifications";
+import Notifications from "../Notifications";
 import Companies from "./Companies";
 import Products from "./Products";
 
